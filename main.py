4b9aa714084e599547ba26e94792277923f278ae
mukul <me.is.mukul.2005@gmail.com>
2024-09-29 15:55:12 +0530

    nothing

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 import together
 import os
+import sys
+import time
 from dotenv import load_dotenv
+
+def typing_effect(text, delay=0.1):
+    for char in text:
+        sys.stdout.write(char)
+        sys.stdout.flush()
+        time.sleep(delay)
+    print() 
 
 load_dotenv()
 samp = []
@@ -20,4 +29,7 @@ for i in range(15):
         message = samp[-2]
     
     completion=client.chat.completions.create(model="meta-llama/Meta-Llama-3.1-8B-Instruct-Turbo",messages=[{"role":"user","content":message}])
-    print(completion.choices[0].message.content)
+    typing_effect((completion.choices[0].message.content), 0.05)
+
+
+
