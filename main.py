46397d415894c6c6be14a2afcccaf922c491947c
mukul <me.is.mukul.2005@gmail.com>
2024-09-29 14:51:59 +0530

    updated

--- main.py
+++ main.py
@@ -13,8 +13,11 @@ for i in range(15):
     samp.append(message)
     if message == "exit":
         break
-    if ("previous" in message) and ("command" in message):
+    if ("previous" in message) and ("command" in message) and ("run" not in message):
         print("your last command was '{}'".format(samp[-2]))
+
+    if ("previous" in message) and ("command" in message) and ("run" in message):
+        message = samp[-2]
     
     completion=client.chat.completions.create(model="meta-llama/Meta-Llama-3.1-8B-Instruct-Turbo",messages=[{"role":"user","content":message}])
     print(completion.choices[0].message.content)
