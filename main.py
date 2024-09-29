4d9d171e350dfb29cbe85d5fc0f7eaee69b0ac45
mukul <me.is.mukul.2005@gmail.com>
2024-09-29 14:43:14 +0530

    previous command feature added

--- main.py
+++ main.py
@@ -3,14 +3,18 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-
+samp = []
 api_key = os.getenv("API_KEY")
 
 client = together.Together(api_key=api_key)
 
 for i in range(15):
     message=str(input(">>> "))
+    samp.append(message)
     if message == "exit":
         break
+    if ("previous" in message) and ("command" in message):
+        print("your last command was '{}'".format(samp[-2]))
+    
     completion=client.chat.completions.create(model="meta-llama/Meta-Llama-3.1-8B-Instruct-Turbo",messages=[{"role":"user","content":message}])
     print(completion.choices[0].message.content)
